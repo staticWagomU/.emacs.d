b7bf6e509086b9a359205a7968855f85ee7c83ad
staticWagomU <to6wa17@gmail.com>
2024-04-10 09:27:55 +0900

    新生活始めるよ

--- earlt-init.el
+++
@@ -1,3 +0,0 @@
-(push '(menu-bar-lines . 0) default-frame-alist) ; no menu bar
-(push '(tool-bar-lines . 0) default-frame-alist) ; no tool bar
-(push '(vertical-scroll-bars) default-frame-alist) ; no scroll bars
