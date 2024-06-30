d9ffdb606b62da13934419f12a02d8146e00e004
staticWagomU <to6wa17@gmail.com>
2024-07-01 08:49:59 +0900

    feat: leafを導入！！！！

--- init.el
+++ init.el
@@ -3,3 +3,27 @@
 (tool-bar-mode -1)
 (global-display-line-numbers-mode 1)
 (setq ring-bell-function 'ignore)
+
+
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
+
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org"   . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu"   . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+
